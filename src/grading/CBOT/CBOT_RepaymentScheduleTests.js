7d8eee640208d0cf3e114631d1aacbdeab646dda
Marcello Delgado <marcellodelgado0149@gmail.com>
2024-02-25 12:54:44 -0600

    Check Totals to Date subtest

--- src/grading/CBOT/CBOT_RepaymentScheduleTests.js
+++ src/grading/CBOT/CBOT_RepaymentScheduleTests.js
@@ -71,7 +71,7 @@ RepaymentScheduleTests = {
                 .getRange(2, 4, CardBalanceOverTimeTests.numRows - 1, 1)
                 .getValues()
                 .flat();
-            
+
             let expectedAfterBalances = unpaidBalances
                 .map((unpaidBal, index) => unpaidBal - payments[index]);
 
@@ -96,5 +96,53 @@ RepaymentScheduleTests = {
             + `\n\t\t      the minimum payment from the unpaid balance?`;
         student.logFeedback(message + errBuffer);
         return result;
-    }
+    },
+
+    CheckTotalsToDate: function (student, cbotTestSheet) {
+        let result = true;
+        let errBuffer = '';
+
+        if (CardBalanceOverTimeTests.numRows <= 1) {
+            result = false;
+            errBuffer += `\n\t\t\tERROR: You don't have enough data`;
+        } else {
+            let actualTotalstd = cbotTestSheet
+                .getRange(2, 5, CardBalanceOverTimeTests.numRows - 1, 1)
+                .getValues()
+                .flat();
+
+            let payments = cbotTestSheet
+                .getRange(2, 3, CardBalanceOverTimeTests.numRows - 1, 1)
+                .getValues()
+                .flat();
+
+            // can we do this better?
+            let expectedTotalstd = [];
+            let partialSum = 0;
+            for (let index = 0; index < CardBalanceOverTimeTests.numRows - 1; index++) {
+                partialSum += payments[index];
+                expectedTotalstd.push(partialSum);
+            }
+
+            let cellNameMatrix = Utils
+                .createCellNameArray(2, 5, CardBalanceOverTimeTests.numRows - 1, 1);
+
+            let badBalances = Utils
+                .createZippedThreeArrayFlat(cellNameMatrix, expectedTotalstd, actualTotalstd)
+                .filter(([_cell, expected, actual]) => Math.abs(expected - actual) > TOLERANCE);
+
+            if (badBalances.length > 0) {
+                result = false;
+                badBalances.forEach(([cell, expected, actual]) => {
+                    errBuffer += `\n\t\t\tERROR: Unpaid balance in ${cell} should be ${Utils.asMoney(expected)},`
+                        + `\n\t\t\t       but instead got ${Utils.asMoney(actual)}`
+                })
+            }
+        }
+
+        let message = `\t\t${result ? 'PASS' : 'FAIL'}: Are totals paid to date the sum of all payments up to`
+            + `\n\t\t      and including that row?`;
+        student.logFeedback(message + errBuffer);
+        return result;
+    },
 }
