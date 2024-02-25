a07f9356a3f2c3fbe1aa40a1b0f98c893325f83d
Marcello Delgado <marcellodelgado0149@gmail.com>
2024-02-25 10:24:51 -0600

    Created space for repayment schedule tests

--- src/grading/CBOT/CardBalanceOverTimeTest.js
+++ src/grading/CBOT/CardBalanceOverTimeTest.js
@@ -16,21 +16,21 @@ CardBalanceOverTimeTests = {
         return finalResult;
     },
 
-    // RepaymentSchedule: function (student, cbotTestSheet) {
-    //     student.logFeedback("\n\tCard Balance Over Time Tests -- Running Repayment Schedule Tests...\n");
-    //     const results = new Array();
-    //     Object.values(RepaymentScheduleTests).forEach((f) => {
-    //         if (typeof f === 'function') {
-    //             results.push(f.call(this, student, cbotTestSheet));
-    //         }
-    //     });
+    RepaymentSchedule: function (student, cbotTestSheet) {
+        student.logFeedback("\n\tCard Balance Over Time Tests -- Running Repayment Schedule Tests...\n");
+        const results = new Array();
+        Object.values(RepaymentScheduleTests).forEach((f) => {
+            if (typeof f === 'function') {
+                results.push(f.call(this, student, cbotTestSheet));
+            }
+        });
 
-    //     let finalResult = results.reduce((b1, b2) => b1 && b2, true);
+        let finalResult = results.reduce((b1, b2) => b1 && b2, true);
 
-    //     let message = finalResult ? "ALL TESTS PASS" : "INCOMPLETE";
-    //     student.logFeedback("\n\tCard Balance Over Time Tests -- Repayment Schedule Tests: " + message + '\n');
-    //     return finalResult;
-    // },
+        let message = finalResult ? "ALL TESTS PASS" : "INCOMPLETE";
+        student.logFeedback("\n\tCard Balance Over Time Tests -- Repayment Schedule Tests: " + message + '\n');
+        return finalResult;
+    },
 
     // SummaryStats: function (student, cbotTestSheet) {
     //     student.logFeedback("\n\tCard Balance Over Time Tests -- Running Summary Stats Tests...\n");
